61eb8db67bc8ca7a94089f2fa0d1fd48ad352ab1
sundhhy <sundhhy@163.com>
2023-03-27 21:34:26 +0800

    首次可编译下载提交

---
+++ hal/lmosemhal_start.c
@@ -0,0 +1,13 @@
+/***************************************
+  开始入口文件lmosemhal_start.c
+  *************************************/
+
+#include "lmosemtypes.h"
+#include "lmosemmctrl.h"
+
+LKHEAD_T void lmosemhal_start()
+{
+		for(;;);
+		return;
+
+}
---
+++ include/script/buildfile.h
@@ -0,0 +1,20 @@
+#ifndef BUILDFILE_H
+#define BUILDFILE_H
+
+#define BUILD_BOOT_OBJS
+#define BUILD_HALY_OBJS init.o lmosemhal_start.o
+#define BUILD_KRNL_OBJS 
+#define BUILD_MEMY_OBJS
+#define BUILD_FSYS_OBJS
+#define BUILD_DRIV_OBJS 
+#define BUILD_LIBS_OBJS 
+#define BUILD_TASK_OBJS  
+#define BUILD_LINK_OBJS BUILD_BOOT_OBJS BUILD_HALY_OBJS
+
+
+#define LINKR_IPUT_FILE BUILD_LINK_OBJS
+#define LINKR_OPUT_FILE lmosemkrnl.elf
+#define KERNL_ELFF_FILE LINKR_OPUT_FILE
+#define KERNL_BINF_FILE lmosemkrnl.bin
+                          
+#endif // BUILDFILE_H
